1de113e2ced3fa3067035d0795b240a9db5f95a4
Jan Dobes <dobesjan97@gmail.com>
2024-07-18 03:33:13 +0200

    JobSchedulerClient

---
+++ DataProcessor/JobSchedulerClient.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Net.Client;
+using JobScheduler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static JobScheduler.JobSchedulerClient;
+
+namespace DataProcessor
+{
+	public class JobSchedulerClient
+	{
+		private readonly JobSchedulerClient _client;
+
+		public JobSchedulerClient(string address)
+		{
+			var channel = GrpcChannel.ForAddress(address);
+			_client = new JobScheduler.JobSchedulerClient(channel);
+		}
+
+		public async Task<ScheduleJobResponse> ScheduleJob(int jobId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<GetJobStatusResponse> GetJobStatus(int jobId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<GetJobResponse> GetJob(string jobname, string pluginName)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
